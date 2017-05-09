751c990097430293e8123b3b3d1e4277f8410ffb
Chris <lily.danisch@gmail.com>
2017-05-09 10:33:15 -0700

    Initial commit with gitignore

---
+++ src/main.scala
@@ -0,0 +1,23 @@
+import scala.annotation.tailrec
+
+/**
+  * Created by lilyd on 5/9/2017.
+  */
+object Main{
+  def main(args: Array[String]): Unit = {
+    val bigFactorial = factorial(100)
+    println(s"Factorial=$bigFactorial")
+
+    val digitSum = bigFactorial.toString.toCharArray.foldLeft(0)((acc: Int, digit: Char) => acc + digit.asDigit)
+    println(s"Sum of digits=$digitSum")
+  }
+
+  @tailrec
+  def factorial(value: BigInt, currentValue: BigInt = BigInt(1)): BigInt = {
+    value match {
+      case n if n == BigInt(1) => currentValue //BigInt doesn't get along with pattern matching
+      case n => factorial(n - 1, n * currentValue)
+    }
+  }
+
+}
